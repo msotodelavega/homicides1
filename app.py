848c7009020054f3f10a774de49f2b0537126adb
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 13:56:54 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import dash
+import dash_labs as dl
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(
+    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.MINTY]
+)
+server = app.server
+
+dl.print_registry()
+navbar = dbc.NavbarSimple(
+    dbc.Nav(
+        [
+            dbc.NavLink(page["name"], href=page["path"])
+            for page in dash.page_registry.values()
+            if page.get("top_nav")
+        ],
+    ),
+    brand="HOMICIDES IN COLOMBIA",
+    color="primary",
+    dark=True,
+    className="mb-2",
+)
+
+app.layout = dbc.Container(
+    [navbar, dl.plugins.page_container],
+    fluid=True,
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)

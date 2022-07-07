49d5825277078759368e26d416288cf1ce52540e
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 11:19:25 -0500

    Update topic_1.py

--- pages/topic_1.py
+++ pages/topic_1.py
@@ -3,8 +3,6 @@ import dash_bootstrap_components as dbc
 
 from dash_labs.plugins import register_page
 from dash import dcc, html, Input, Output, callback
-from .side_bar import sidebar
-from app_dataframe import df_year_month
 
 register_page(
     __name__,

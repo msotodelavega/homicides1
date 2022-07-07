7c7d0223a20b13a7ba8feebcdfa823260053d1f6
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 11:28:20 -0500

    Update topic_6.py

--- pages/topic_6.py
+++ pages/topic_6.py
@@ -5,7 +5,6 @@ import dash_bootstrap_components as dbc
 from dash_labs.plugins import register_page
 from dash import dcc, html, Input, Output, callback
 from .side_bar import sidebar
-from app_dataframe import df_dpto_day, df_dpto_month, df_dpto_year
 
 register_page(__name__,name='word cloud')
 

99c53a33b8f8a1ee11d27301d75562fb630a9e4c
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 16:18:49 -0500

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -52,15 +52,32 @@ def layout():
 # Create figure
 def graficar(city_dropdown):
 
-    df = df_hom[['fecha','cantidad']]
-
     city = city_dropdown
 
+    df = df_hom[['fecha','cantidad']]
+    
+    df = df.set_index('fecha')
+    df = df.resample('M').sum()
+    df.columns = ['total']
+    df['month_index'] = df.index.month
+
+    name = 'model/SARIMAX_total.pkl'
+
+    with open(name, 'rb') as pkl:
+        SARIMA_model = pickle.load(pkl)
+        fitted_series, lower_series ,upper_series = sarimax_forecast_city(SARIMA_model,df, 5)
+
+    df = df.reset_index()
+    df_fitted = fitted_series.to_frame().reset_index()
+    df_fitted.columns = ['fecha','total']
     
     fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(x=[3,4,5,6], y=[3,4,5,6],
+    fig2.add_trace(go.Scatter(x=df['fecha'], y=df['total'],
                         #mode='lines',
                         name='original'))
+    fig2.add_trace(go.Scatter(x=df_fitted['fecha'], y=df_fitted['total'],
+                        #mode='lines',
+                        name='fitted'))
     fig2.update_layout(
         showlegend=False,
         title_text='Forecasting homicides in '+city,

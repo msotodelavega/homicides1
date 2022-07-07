9b96e8b5c80b30759d833cce028296a922d16e00
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 11:22:54 -0500

    Update topic_1.py

--- pages/topic_1.py
+++ pages/topic_1.py
@@ -4,6 +4,8 @@ import dash_bootstrap_components as dbc
 from dash_labs.plugins import register_page
 from dash import dcc, html, Input, Output, callback
 
+from .side_bar import sidebar
+
 register_page(
     __name__,
     name='Time series',

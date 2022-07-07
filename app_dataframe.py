75577d7b3ced6f0b9fb794a90bab219477dedda3
AlexisBerrio <bealexis@hotmail.com>
2022-07-07 15:04:26 -0500

    First code documentation

--- app_dataframe.py
+++ app_dataframe.py
@@ -4,13 +4,6 @@ import numpy as np
 import json
 import jellyfish
 
-#---
-
-# Standard Imports
-import pandas as pd
-import numpy as np
-import json
-import jellyfish
 
 # Import data
 cod_dane= pd.read_csv('data/cod_dane.csv')
@@ -34,6 +27,7 @@ df = df[df["departamento"].isin(keep_values)]
 df['edad_grupo'].replace(' ', np.nan, inplace=True)
 df = df.dropna()
 
+# Dict to replace several weapon names
 dict_values =  {
 "ARMA BLANCA / CORTOPUNZANTE":"ARMA BLANCA",
 "ARMA DE FUEGO":"ARMA DE FUEGO",
@@ -73,64 +67,67 @@ dict_values =  {
 "PRENDAS DE VESTIR":"OTRAS ARMAS",
 "LIQUIDOS":"OTRAS ARMAS"
 }
-
 df = df.replace({"arma": dict_values})
 
+# Create year column
 df['ano'] = df['fecha'].dt.year
+# Create week column
+df['week'] = df['fecha'].dt.to_period('M')
+# Create dayname column as Monday, Tuesday...
 df["dia"] = df["fecha"].dt.day_name
+#Reorder columns index
 df = df.reindex(columns=['fecha','ano','dia','departamento','municipio','codigo', 'arma', 'genero', 'edad_grupo', 'cantidad'])
 
-#para grafica por año
+# Group data to plot by year
 df_yearly = df.groupby("ano")['cantidad'].sum()
 df_yearly = df_yearly.to_frame().reset_index()
 
-#para grafica por mes
+# Group data to plot by month
 df['mes'] = df['fecha'].dt.month
 df_monthly = df.groupby("mes")['cantidad'].sum()
 df_monthly = df_monthly.to_frame().reset_index()
 
-#para grafica por dia
+# Group data to plot by day
 df_daily = df.groupby("dia")['cantidad'].sum()
 df_daily = df_daily.to_frame().reset_index()
 
-#para grafica de serie
+# Group data to plot timeserie by months
 df['ano_mes'] = df['fecha'].dt.to_period('M').astype(dtype=str)
 df_year_month = df.groupby("ano_mes")['cantidad'].sum()
 df_year_month = df_year_month.to_frame().reset_index()
 
-#para grafica de rangos de edad
+# Group data to plot by age groups
 df_age = df.groupby("edad_grupo")['cantidad'].sum()
 df_age = df_age.to_frame().reset_index()
 
-#para grafico por genero
+# Group data to plot by genre
 df_gen = df.groupby("genero")['cantidad'].sum()
 df_gen = df_gen.to_frame().reset_index()
 
-#para grafico por tipo de arma
+# Group data to plot by type of weapon
 df_weapon = df.groupby("arma")['cantidad'].sum()
 df_weapon = df_weapon.to_frame().reset_index()
 
-#para grafica box plot por departamento
-df['week'] = df['fecha'].dt.to_period('M')
+# Group data to show a boxplot by department and weekly
 df_departamento = df.groupby(["departamento","week"])['cantidad'].sum()
 df_departamento = df_departamento.to_frame().reset_index()
 
-#para grafica box plot municipio
+# Group data to show a boxplot by municipality montly
 df['week'] = df['fecha'].dt.to_period('M')
 df_municipio = df.groupby(["municipio","week","departamento"])['cantidad'].sum()
 df_municipio = df_municipio.to_frame().reset_index()
 df_municipio = df_municipio.sort_values(by='cantidad', ascending=False)
 df_municipio = df_municipio[df_municipio['cantidad'] > 20]
 
-#para grafico departamento x año
+# Group data to plot by department yearly
 df_dpto_year = df.groupby(["ano","departamento"])['cantidad'].sum()
 df_dpto_year = df_dpto_year.reset_index()
 
-#para grafico departamento x mes
+# Group data to plot by department montly
 df_dpto_month = df.groupby(["mes","departamento"])['cantidad'].sum()
 df_dpto_month = df_dpto_month.reset_index()
 
-#para grafico departamento x día
+# Group data to plot by department daily
 df_dpto_day = df.groupby(["dia","departamento"])['cantidad'].sum()
 df_dpto_day = df_dpto_day.reset_index()
 

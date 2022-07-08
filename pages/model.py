3a348fbbeb926211236293041b577f5cadafffb7
msotodelavega <105311939+msotodelavega@users.noreply.github.com>
2022-07-07 19:08:00 -0500

    cambios6

--- pages/model.py
+++ pages/model.py
@@ -3,7 +3,6 @@ from dash_labs.plugins import register_page
 
 import dash_bootstrap_components as dbc
 import pandas as pd
-import pickle
 import plotly.graph_objects as go
 
 from app_dataframe import df_hom
@@ -13,24 +12,6 @@ register_page(
     top_nav=True,
     order=5
     )
-
-def sarimax_forecast_city(SARIMAX_model, df, periods):
-    # Forecast
-    n_periods = periods
-
-    forecast_df = pd.DataFrame({"month_index":pd.date_range(df.index[-1], periods = n_periods, freq='MS').month},
-                    index = pd.date_range(df.index[-1] + pd.DateOffset(months=1), periods = n_periods, freq='MS'))
-
-    fitted, confint = SARIMAX_model.predict(n_periods=n_periods, 
-                                            return_conf_int=True,
-                                            exogenous=forecast_df[['month_index']])
-    index_of_fc = pd.date_range(df.index[-1] + pd.DateOffset(months=1), periods = n_periods, freq='MS')
-
-    # make series for plotting purpose
-    fitted_series = pd.Series(fitted, index=index_of_fc)
-    lower_series = pd.Series(confint[:, 0], index=index_of_fc)
-    upper_series = pd.Series(confint[:, 1], index=index_of_fc)
-    return fitted_series, lower_series, upper_series
 
 def layout():
     return dbc.Row([
@@ -82,10 +63,6 @@ def graficar(city_dropdown):
     else:
         name = 'model/SARIMAX_total.pkl'
         pred_med = pd.read_excel('data/pred_tot.xlsx')
-
-    with open(name, 'rb') as pkl:
-        SARIMA_model = pickle.load(pkl)
-        fitted_series, lower_series ,upper_series = sarimax_forecast_city(SARIMA_model,df, 5)
 
     df = df.reset_index()
 
